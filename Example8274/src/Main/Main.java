10d174de3cd1a2bbe9fec384cf40d33f6c732b74
Aarya-Shinde <148617878+Aarya-Shinde@users.noreply.github.com>
2024-07-01 09:53:53 +0530

    Add files via upload

---
+++ Example8274/src/Main/Main.java
@@ -0,0 +1,125 @@
+//
+//package Main;
+//
+//import java.util.Scanner;
+//
+//
+// 
+// 
+//public class Main {
+//
+//	
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		
+//		Scanner sc=new Scanner(System.in);
+//				
+//		System.out.println("Enter the value of radius,height");
+//		
+//		int r=sc.nextInt();
+//		double h=sc.nextDouble();
+//		double V=3.14*r*r*h;
+//		
+//	System.out.println("Volume of Cylinder" + V);
+//
+//	}
+//
+//}
+//
+
+
+
+
+
+
+
+
+
+
+
+//
+//package Main;
+//
+//
+//public class Main {
+//
+//	
+//	public static void main(String[] args) {
+//		
+//		int number = 8; 
+//		{
+//			
+//			if(number%2==0)
+//			{
+//				System.out.println("This is Even Number");
+//			}
+//			else 
+//			{
+//				System.out.println("Odd Number");
+//			}
+//		}
+//	}
+//}
+//
+
+
+
+
+//
+//package Main;
+//
+//
+//public class Main {
+//
+//	
+//	public static void main(String[] args) {
+//		
+//		int number = 2024; 
+//		{
+//			
+//			if(number%4==0)
+//			{
+//				System.out.println("Leap Year!!!");
+//			}
+//			else 
+//			{
+//				System.out.println("Normal Year");
+//			}
+//		}
+//	}
+//}
+//
+
+
+
+package Main;
+
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		
+		int yearr = 2024; 
+		int x=15, y=9, z=2;
+		
+			
+			if((x>y) && (x>z))
+			{
+				System.out.println("X is greater number");
+			}
+			else if ((y>x) &&(y>x))
+			{
+				System.out.println("Y is greater number");
+			}
+			else 
+			{
+				System.out.println("Z is greater number");
+			}
+		}
+	}
+
+
+
+
+
